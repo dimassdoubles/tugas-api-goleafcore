807e8e4d2ffaee55997f783ba66abf82b6698b10
Dimas Saputro <codedimas@gmail.com>
2023-05-17 11:34:53 +0700

    Memperbaiki penulisan meta struct

--- pkg/learngo/tables/penjualan.go
+++ pkg/learngo/tables/penjualan.go
@@ -6,10 +6,10 @@ import (
 )
 
 type Penjualan struct {
-	PenjualanId     int64           `json:"penjualanId" example="10"`
-	TotalPenjualan  decimal.Decimal `json:"totalPenjualan" example="120000"`
-	TotalPembayaran decimal.Decimal `json:"totalPembayaran" example="150000"`
-	TotalKembalian  decimal.Decimal `json:"totalKembalian" example="30000"`
+	PenjualanId     int64           `json:"penjualanId" example:"10"`
+	TotalPenjualan  decimal.Decimal `json:"totalPenjualan" example:"120000"`
+	TotalPembayaran decimal.Decimal `json:"totalPembayaran" example:"150000"`
+	TotalKembalian  decimal.Decimal `json:"totalKembalian" example:"30000"`
 
 	glentity.BaseEntity
 }
